9386bcbf54c20b95823b05e0befa8bdcf8b74771
Simon Davis <simon_freelance@hotmail.com>
2019-02-12 16:13:08 -0500

    Initial setup.

---
+++ src/main/java/com/lambdaschool/restdogs/RestDogsApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.restdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDogsApplication.class, args);
+    }
+
+}
+
